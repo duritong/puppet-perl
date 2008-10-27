d081f22b4e595cbaa1e6262cbd6a2586d1ed15a6
mh <mh@immerda.ch>
2008-10-27 11:21:45 +0000

    cleaned up module

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,11 +2,8 @@
 # Copyright (C) 2007 admin@immerda.ch
 # GPLv3
 
-# modules_dir { "perl": }
-
 class perl {
     case $operatingsystem {
-        gentoo: { include perl::gentoo }
         default: { include perl::base }
     }
 }
@@ -18,26 +15,6 @@ class perl::base {
 
 }
 
-class perl::gentoo inherits perl::base {
-    Package[perl]{
-        category => 'some-category',
-    }
-
-    #conf.d file if needed
-    # needs module gentoo
-    #gentoo::etcconfd { perl: require => "Package[perl]", notify => "Service[perl]"}
-}
-
 define perl::module () {
     package { "${name}": ensure => installed }
-    case $operatingsystem {
-        gentoo: { perl::module::gentoo {"${name}": } }
-        # default: { include perl::base }
-    }
-}
-
-define perl::module::gentoo {
-    Package["${name}"]{
-        category => 'some-category',
-    }
 }
