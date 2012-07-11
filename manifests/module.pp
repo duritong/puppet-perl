74853d6c8247358f647b40995d770bdc292bfca1
Micah Anderson <micah@riseup.net>
2012-07-11 15:01:30 -0400

    add operatingsystem configuration for debian, keeping the existing as the default

--- manifests/module.pp
+++ manifests/module.pp
@@ -1,7 +1,14 @@
-define perl::module (
-  $ensure = 'present'
-) {
+define perl::module ( $ensure = 'present' )
+{
   if $ensure == 'present' { require perl }
-  package {"perl-${name}": ensure => $ensure }
+
+  case $operatingsystem {
+    'debian','ubuntu': {
+        package {"lib${name}-perl": ensure => $ensure }
+    }
+    default: {
+      package {"perl-${name}": ensure => $ensure }
+    }
+  }
 }
 
