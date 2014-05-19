9086abf53ea561b88c34e24f1992ade88e29d0ef
tr <tr@subpage.ch>
2014-05-19 18:05:10 +0200

    change char to us-ascii

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@
 # Copyright (C) 2007 admin(at)immerda.ch
 # GPLv3
 # adapted by Puzzle ITC
-# Marcel Härry haerry+puppet(at)puzzle.ch
+# Marcel Haerry haerry+puppet(at)puzzle.ch
 # Simon Josi josi+puppet(at)puzzle.ch
 
 class perl {
